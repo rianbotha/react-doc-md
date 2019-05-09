6b07b8fb0fbff98e950d8ffe76c8ef4fa0cb625f
rianbotha <hello@rian.co.za>
2019-05-09 16:46:02 +0200

    Fix local name for class components

--- src/local-name-handler.js
+++ src/local-name-handler.js
@@ -2,7 +2,11 @@ const localNameHandler = (doc, def) => {
   if (def.parentPath.value.type === 'ExportDefaultDeclaration') {
     doc.set('localName', 'default');
   } else {
-    doc.set('localName', def.parentPath.value.id.name);
+    if (def.value.type = 'ClassDeclaration') {
+      doc.set('localName', def.value.id.name);
+    } else {
+      doc.set('localName', def.parentPath.value.id.name);
+    }
   }
 
   return doc;
