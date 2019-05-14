0c322581439592dbb87fc6d645dd6b172ec3f5ba
rianbotha <hello@rian.co.za>
2019-05-14 08:37:55 +0200

    Fix local name issue

--- src/local-name-handler.js
+++ src/local-name-handler.js
@@ -2,7 +2,7 @@ const localNameHandler = (doc, def) => {
   if (def.parentPath.value.type === 'ExportDefaultDeclaration') {
     doc.set('localName', 'default');
   } else {
-    if (def.value.type = 'ClassDeclaration') {
+    if (def.value.type === 'ClassDeclaration') {
       doc.set('localName', def.value.id.name);
     } else {
       doc.set('localName', def.parentPath.value.id.name);
